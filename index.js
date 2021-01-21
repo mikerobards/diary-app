20d45832a9d3ce99a8e78b3cc73db8562451d305
Mike Robards <mikerobards@icloud.com>
2021-01-21 15:17:38 -0500

    add entry section

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const entryForm = document.getElementById('entryForm')
+const entriesSection = document.getElementById('entries')
+const entryTextbox = document.getElementsByClassName('entry-textbox')
 
-console.log(entryForm)
+
 
 function addEntryToDOM(event) {
     event.preventDefault();
+    const entryDiv = document.createElement('div')
+    // entryDiv.classList.add('single-entry')
+    entryDiv.className = 'single-entry'
+    entryDiv.innerHTML = entryTextbox[0].value
+    // console.log(entryTextbox[0].value)
+    entriesSection.appendChild(entryDiv)
 }
 
 entryForm.addEventListener('submit', addEntryToDOM)
