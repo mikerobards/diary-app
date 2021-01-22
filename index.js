d25548a993a88ba7b80a9927848794445a83a378
Mike Robards <mikerobards@icloud.com>
2021-01-22 12:30:25 -0500

    add entries nav button

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const entryForm = document.getElementById('entryForm')
-const entriesSection = document.getElementById('entries')
-const entryTextbox = document.getElementsByClassName('entry-textbox')
+const entriesSection = document.querySelector('#entries')
+const entryTextbox = document.querySelector('.entry-textbox')
+const entriesNav = document.querySelector('.entries-nav')
+let count = 1
 
 
 
@@ -8,9 +10,16 @@ function addEntryToDOM(event) {
     event.preventDefault();
     const entryDiv = document.createElement('div')
     entryDiv.className = 'single-entry'
-    entryDiv.innerHTML = entryTextbox[0].value
+    entryDiv.innerHTML = entryTextbox.value
+    entryDiv.style.display = 'none'
     entriesSection.appendChild(entryDiv)
-    entryTextbox[0].value = '';
+    entryTextbox.value = ''
+
+    const displayEntryButton = document.createElement('button')
+    displayEntryButton.className = 'display-entry-button'
+    displayEntryButton.innerText = count
+    entriesNav.appendChild(displayEntryButton)
+    count++
 }
 
 entryForm.addEventListener('submit', addEntryToDOM)
