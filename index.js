c3b53c69574cdce1fadca513c3c4236c53c63a3f
Mike Robards <mikerobards@icloud.com>
2021-01-22 12:08:14 -0500

    style entries section

--- index.js
+++ index.js
@@ -7,11 +7,10 @@ const entryTextbox = document.getElementsByClassName('entry-textbox')
 function addEntryToDOM(event) {
     event.preventDefault();
     const entryDiv = document.createElement('div')
-    // entryDiv.classList.add('single-entry')
     entryDiv.className = 'single-entry'
     entryDiv.innerHTML = entryTextbox[0].value
-    // console.log(entryTextbox[0].value)
     entriesSection.appendChild(entryDiv)
+    entryTextbox[0].value = '';
 }
 
 entryForm.addEventListener('submit', addEntryToDOM)
